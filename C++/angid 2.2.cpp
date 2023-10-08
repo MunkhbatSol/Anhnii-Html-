a5a87f0bdae408dc72074bef9b1608064983cfd7
Amaru114 <146306608+Amaru114@users.noreply.github.com>
2023-10-08 19:56:52 +0800

    Delete C++ directory

--- C++/angid 2.2.cpp
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-int main()
-{
-	int a, b, c;
-	scanf("%d%d", &a, &b);
-	c = (a > b) * a + (a <= b) *b;
-	printf("%d\n", c);
-}
--- C++/angid 3.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-int main()
-{
-int y, m, d;
-printf("tursun on saraa hiine uu:");
-scanf("%d %d %d", &y, &m, &d);
-printf("tanii tursun udur: %d/%d/%d.", y, m, d);
-return 0;
-}
